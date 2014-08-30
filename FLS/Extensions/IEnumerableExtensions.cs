dbfe6a0c67666e488e1146ef642caa21a7eb95f6
david grupp <david@grupp.me>
2014-08-29 19:53:26 -0500

    added license for each file

--- FLS/Extensions/IEnumerableExtensions.cs
+++ FLS/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿#region License
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -8,12 +24,44 @@ namespace FLS
 {
 	public static class IEnumerableExtensions
 	{
+		/// <summary>
+		/// Returns all distinct elements of the given source, where "distinctness"
+		/// is determined via a projection and the default equality comparer for the projected type.
+		/// </summary>
+		/// <remarks>
+		/// This operator uses deferred execution and streams the results, although
+		/// a set of already-seen keys is retained. If a key is seen multiple times,
+		/// only the first element with that key is returned.
+		/// </remarks>
+		/// <typeparam name="TSource">Type of the source sequence</typeparam>
+		/// <typeparam name="TKey">Type of the projected element</typeparam>
+		/// <param name="source">Source sequence</param>
+		/// <param name="keySelector">Projection for determining "distinctness"</param>
+		/// <returns>A sequence consisting of distinct elements from the source sequence,
+		/// comparing them by the specified key projection.</returns>
 		public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
 			Func<TSource, TKey> keySelector)
 		{
 			return source.DistinctBy(keySelector, null);
 		}
 
+		/// <summary>
+		/// Returns all distinct elements of the given source, where "distinctness"
+		/// is determined via a projection and the specified comparer for the projected type.
+		/// </summary>
+		/// <remarks>
+		/// This operator uses deferred execution and streams the results, although
+		/// a set of already-seen keys is retained. If a key is seen multiple times,
+		/// only the first element with that key is returned.
+		/// </remarks>
+		/// <typeparam name="TSource">Type of the source sequence</typeparam>
+		/// <typeparam name="TKey">Type of the projected element</typeparam>
+		/// <param name="source">Source sequence</param>
+		/// <param name="keySelector">Projection for determining "distinctness"</param>
+		/// <param name="comparer">The equality comparer to use to determine whether or not keys are equal.
+		/// If null, the default equality comparer for <c>TSource</c> is used.</param>
+		/// <returns>A sequence consisting of distinct elements from the source sequence,
+		/// comparing them by the specified key projection.</returns>
 		public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
 			Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
 		{
@@ -25,9 +73,7 @@ namespace FLS
 		private static IEnumerable<TSource> DistinctByImpl<TSource, TKey>(IEnumerable<TSource> source,
 			Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
 		{
-
 			return source.GroupBy(keySelector, comparer).Select(g => g.First());
-
 		}
 	}
 }
